1831516e71c42530306566780966fedaeb50a631
hoang <trandanghoang157202@gmail.com>
2022-10-06 22:28:11 +0700

    upload source code

---
+++ main.js
@@ -0,0 +1,9 @@
+// $(".size").on('click', function(){
+//     $(this).toggleClass('focus').siblings().removeClass('focus');
+//  })
+
+
+ var heart = document.querySelector('.like i')
+ heart.addEventListener('click', function(e){
+    heart.style.color = 'red'
+ })
